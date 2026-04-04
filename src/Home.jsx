6e0f206f11c8b5754fc4ebb9198a3cf8f3990478
Omar <oh88290omar@gmail.com>
2026-04-04 04:55:08 +0200

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+export default function Home() {
+  return (
+    <>
+    <div className="nav bg-black  max-w-full h-screen ">
+    <nav className='text-main-500 bigFont items-center'>
+      <ul className='text-center flex flex-row justify-center py-5 text-xl'>
+        <h3 className=''>blogsOfCode</h3>
+        <li className='px-10'>Home</li>
+        <li className='px-10'>Articles</li>
+        <li className='px-10'>Contact</li>
+
+
+      </ul>
+    </nav>
+    </div>
+    </>
+  )
+}
